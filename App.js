d6e694931c597be8b1e92bacf64c04bbb56c6559
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-06-16 14:12:25 +1000

    get top 20 stories from hacker news

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  async getTopStories() {
+    let data = [];
+    try {
+      let response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+      let responseJson = await response.json();
+
+      for (let i = 0; i < 20; i++) { //responseJson.length
+        let id = responseJson[i];
+        let itemResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+        let itemResponseJson = await itemResponse.json();
+        data.push(itemResponseJson);
+      }
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async componentDidMount() {
+    let stories = await this.getTopStories();
+    this.setState({
+      isLoading: false,
+      dataSource: stories,
+    });
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.score} - {item.title}</Text>}
+          keyExtractor={(item, index) => `${index}`}
+        />
       </View>
     );
   }
@@ -19,5 +61,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
 });
